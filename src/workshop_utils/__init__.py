015eb336ca2c45d29b438ee421c9c9b4c549e3d7
William F. Broderick <billbrod@gmail.com>
2025-11-10 12:09:01 -0600

    don't have jax preallocate memory

--- src/workshop_utils/__init__.py
+++ src/workshop_utils/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+
 from .fetch import DOWNLOADABLE_FILES, fetch_data, fetch_all
 from .plotting import *
 
-import os
 import pathlib
 
 repo_dir = pathlib.Path(__file__).parent.parent.parent
